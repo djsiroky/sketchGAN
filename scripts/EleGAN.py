85f40ad43e6229df6b9a21907fa12d20598beea7
Dan Siroky <dan.siroky@hok.com>
2019-10-09 10:35:24 -0500

    change filetype to py

---
+++ scripts/EleGAN.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+kernel = np.ones((2,2), np.uint8) 				###Increase the (2,2) for thicker lines. 
+
+i = 1
+while i < 75:							###Depends on the number of images
+	img = cv2.imread("Hack (" + str(i) + ").jpg")
+	edges = 255-cv2.Canny(img, 300, 300)			###May need to change the numbers to get perfect results
+	erosion = cv2.erode(edges, kernel, iterations=1) 
+	cv2.imwrite("Disc_" + str(i) + ".jpg", erosion)
+	i += 1
+
